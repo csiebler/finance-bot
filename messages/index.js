0f34d5c9feb064cd7401057d260c332b45c8d9df
Clemens Siebler <clemens.siebler@gmail.com>
2018-03-19 17:10:54 +0100

    Hacked in Table storage for persisting expenses

--- messages/index.js
+++ messages/index.js
@@ -1,25 +1,31 @@
 "use strict";
 var builder = require("botbuilder");
-var azure = require("botbuilder-azure");
+var azureBot = require("botbuilder-azure");
+var azureStorage = require("azure-storage");
 var path = require('path');
 
 require('dotenv').config();
 
-var tableName = process.env.TABLE_NAME;
 var storageName = process.env.STORAGE_ACCOUNT;
 var storageKey = process.env.STORAGE_KEY;
+
+var tableName = process.env.TABLE_NAME;
+var expensesTableName = process.env.EXPENSES_TABLE_NAME;
+
 var useEmulator = (process.env.NODE_ENV == 'development');
 
+var tableClient = azureStorage.createTableService(storageName, storageKey);
+
 // Connect bot
-var connector = useEmulator ? new builder.ChatConnector() : new azure.BotServiceConnector({
+var connector = useEmulator ? new builder.ChatConnector() : new azureBot.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
 // Setup bot persistency layer
-var azureTableClient = new azure.AzureTableClient(tableName, storageName, storageKey);
-var tableStorage = new azure.AzureBotStorage({gzipData: false}, azureTableClient);
+var azureTableClient = new azureBot.AzureTableClient(tableName, storageName, storageKey);
+var tableStorage = new azureBot.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
@@ -43,16 +49,16 @@ bot.on('conversationUpdate', function (message) {
 
 // Welcome back/query name dialog
 bot.dialog('/', [
-    function(session){
+    function (session) {
         session.beginDialog('greeting');
     },
-    function(session) {
+    function (session) {
         session.beginDialog('selectAction');
     }
 ]);
 
 bot.dialog('greeting', [
-    function(session) {
+    function (session) {
         var username = session.userData.userName;
         if (!username) {
             builder.Prompts.text(session, "What is your name?");
@@ -60,7 +66,7 @@ bot.dialog('greeting', [
             session.endDialog("Welcome back, %s", username);
         }
     },
-    function(session, results) {
+    function (session, results) {
         if (results && results.response) {
             session.userData.userName = results.response;
             session.send("Welcome %s! I'll remember your name from now on!", session.userData.userName);
@@ -72,57 +78,72 @@ bot.dialog('greeting', [
 var options = {
     'Add expense': 'addExpense',
     'Show recent expenses': 'showExpenses',
-    'Show monthly spend': 'showSpend' 
+    'Show monthly spend': 'showSpend'
 };
 
 var style = { listStyle: builder.ListStyle.button };
 
 bot.dialog('selectAction', [
-    function(session) {
+    function (session) {
         builder.Prompts.choice(session, "How can I help you?", Object.keys(options), style);
     },
-    function(session, results) {
+    function (session, results) {
         var action = options[results.response.entity];
         return session.beginDialog(action);
     },
-    function(session) {
+    function (session) {
         // keep going forever
         session.replaceDialog('selectAction');
     }
 ]);
 
 bot.dialog('addExpense', [
-    function(session) {
+    function (session) {
         builder.Prompts.text(session, "Ok, let's log an expense - where did you purchase something?");
     },
-    function(session, results) {
+    function (session, results) {
         session.dialogData.vendor = results.response;
         builder.Prompts.number(session, "How much did you spend?");
     },
-    function(session, results) {
+    function (session, results) {
         session.dialogData.price = results.response;
         var vendor = session.dialogData.vendor;
         var price = session.dialogData.price;
         var message = `You spent ${price} at ${vendor}, is this correct?`;
         builder.Prompts.choice(session, message, "Yes|No", style);
-    },function(session, results) {
+    }, function (session, results) {
         if (results.response.entity.match(/yes/i)) {
-            session.endDialog("Expenses added!");  
+            var userId = session.message.user.id;
+            var channelId = session.message.address.channelId;
+            var vendor = session.dialogData.vendor;
+            var price = session.dialogData.price;
+            addExpenseToTable(userId, channelId, vendor, price);
+            session.endDialog("Expenses added!");
         } else {
-            session.endDialog("Discarded!");  
+            session.endDialog("Discarded!");
         }
     }
 ]);
 
 bot.dialog('showExpenses', [
-    function(session) {
-        session.send("You logged the following purchases: aaaa");
-        session.endDialog("That's all for now!");
+    function (session) {
+        var userId = session.message.user.id;
+        var channelId = session.message.address.channelId;
+
+        session.sendTyping();
+
+        getLastExpensesFromTable(userId, channelId, function(e) {
+            session.send(`Expense at ${e.vendor} for ${e.price}`);
+        });
+
+        setTimeout(function () {
+            session.endDialog();
+        }, 3000); 
     }
 ]);
 
 bot.dialog('showSpend', [
-    function(session) {
+    function (session) {
         session.endDialog("Your current spent in this month is $123");
     }
 ]);
@@ -130,10 +151,50 @@ bot.dialog('showSpend', [
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
-    server.listen(3978, function() {
+    server.listen(3978, function () {
         console.log('Bot up and running at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
 } else {
     module.exports = connector.listen();
 }
+
+function addExpenseToTable(userId, channelId, vendor, price) {
+    var entGen = azureStorage.TableUtilities.entityGenerator;
+    var task = {
+        PartitionKey: entGen.String(userId + '_' + channelId),
+        RowKey: entGen.String(String(Date.now())),
+        vendor: entGen.String(vendor),
+        price: entGen.Int32(price)
+    };
+
+    tableClient.insertEntity(expensesTableName, task, function (error, result, response) {
+        if (!error) {
+            console.log("persisted expense to table");
+        }
+    });
+};
+
+function getLastExpensesFromTable(userId, channelId, action) {
+    var query = new azureStorage.TableQuery()
+        .select(['vendor', 'price', 'RowKey'])
+        .top(5)
+        .where('PartitionKey eq ?', userId + '_' + channelId);
+
+    tableClient.queryEntities(expensesTableName, query, null, function (error, result, response) {
+        if (!error) {
+            result.entries.forEach(function(e){
+                var item = {
+                    price: e.price._,
+                    vendor: e.vendor._,
+                    ts: e.RowKey._
+                };
+                console.log("item: " + JSON.stringify(item));
+                action(item);
+            });
+
+        } else {
+            console.error("something went wrong! " + error);
+        }
+    });
+};
