a0794139d3c9dc18026cee9c86b57168b15b9f66
Clemens Siebler <clemens.siebler@gmail.com>
2018-03-19 10:19:03 +0100

    Added table storage for bot state

--- messages/index.js
+++ messages/index.js
@@ -1,26 +1,28 @@
 "use strict";
 var builder = require("botbuilder");
-var botbuilder_azure = require("botbuilder-azure");
+var azure = require("botbuilder-azure");
 var path = require('path');
 
+require('dotenv').config();
+
+var tableName = process.env.TABLE_NAME;
+var storageName = process.env.STORAGE_ACCOUNT;
+var storageKey = process.env.STORAGE_KEY;
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+var connector = useEmulator ? new builder.ChatConnector() : new azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
     appPassword: process.env['MicrosoftAppPassword'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-//var tableName = 'botdata';
-//var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-//var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-var inMemoryStorage = new builder.MemoryBotStorage();
-
+// Setup bot persistency layer
+var azureTableClient = new azure.AzureTableClient(tableName, storageName, storageKey);
+var tableStorage = new azure.AzureBotStorage({gzipData: false}, azureTableClient);
 
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
-bot.set('storage', inMemoryStorage);
+bot.set('storage', tableStorage);
 
 bot.dialog('/', function (session) {
     session.send('You said ' + session.message.text);
